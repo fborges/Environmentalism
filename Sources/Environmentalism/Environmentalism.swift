5c063b69942ae2d8589a9658e1099f8206dfcc7f
fborges <feliborgez@gmail.com>
2019-02-12 01:49:47 -0200

    feat: init environment with file name

--- Sources/Environmentalism/Environmentalism.swift
+++ Sources/Environmentalism/Environmentalism.swift
@@ -1,4 +1,4 @@
-// Created by Felipe Borges on early 2019
+// Created by Felipe Borges
 // 🐙
 
 import Foundation
@@ -8,10 +8,31 @@ enum EnvError: Error {
   case InvalidFormat
 }
 
+/// An abstraction that takes care of handling variables contained into your DotEnv files.
 public struct Environment {
   
-  public init(filename: String = ".env") throws {
-    try loadEnvironment(filename)
+  private var env: [String : String] = [:]
+  
+  /**
+   Creates an Environment with a relative file name.
+   
+   - Parameter fileName: the name of the DotEnv file. Defaults to `.env`.
+   
+   - Throws: `EnvError.FileNotFound`
+              if `fileName` doesn't correspond to an existing file in the current directory path.
+  */
+  public init(fileName: String = ".env") throws {
+    let fileManager = FileManager.default
+    let currentPath = fileManager.currentDirectoryPath
+    let filePath = "\(currentPath)/\(fileName)"
+    if !fileManager.fileExists(atPath: filePath) {
+      throw EnvError.FileNotFound
+    }
+    
+    let contents = try String(contentsOfFile: filePath, encoding: .utf8)
+    try loadEnvironment(contents: contents)
+  }
+  
   }
   
   func loadEnvironment(_ filename: String) throws {
